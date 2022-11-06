61ddb64edea9cd4d40cd55691504cf376a43076f
Meshack <mbuvi.meshack99@gmail.com>
2022-11-06 15:22:31 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,20 @@
-class Dog
+require "pry"
+require "active_record"
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/test.splite3"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS test (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+
+class Dog < ActiveRecord::Base
 end
